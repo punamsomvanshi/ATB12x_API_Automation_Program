f15aefa2a411b577438590d7be10c3703ccacb92
punamsomvanshi <punamsomvanshi29@gmail.com>
2025-08-06 16:43:28 +0530

    Basic Framework Setup

---
+++ src/test/java/com/testingacdemy/sample/SampleTest.java
@@ -0,0 +1,10 @@
+package com.testingacdemy.sample;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test
+        public void demoTest(){
+        System.out.println("TestNG is Working");
+    }
+}
